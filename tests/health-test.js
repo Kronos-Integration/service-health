0435873d34168f7bde7aacb44e8760b6393a7724
Markus Felten <Markus.Felten@gmx.de>
2017-10-22 14:39:11 +0200

    refactor: rename src/ServiceHealthCheck.js -> src/service-health-check.js

--- tests/health-test.js
+++ tests/health-test.js
@@ -7,51 +7,63 @@ const chai = require('chai'),
   assert = chai.assert,
   expect = chai.expect,
   should = chai.should(),
+  { SendEndpoint, ReceiveEndpoint } = require('kronos-endpoint'),
+  { Service, ServiceProviderMixin } = require('kronos-service'),
   {
-    SendEndpoint, ReceiveEndpoint
-  } = require('kronos-endpoint'),
-  {
-    Service, ServiceProviderMixin
-  } = require('kronos-service'),
-  {
-    ServiceHealthCheck, registerWithManager
-  } = require('../dist/module.js');
+    ServiceHealthCheck,
+    registerWithManager
+  } = require('../dist/service-health-check.js');
 
 class ServiceProvider extends ServiceProviderMixin(Service) {}
-
 const sp = new ServiceProvider();
 
 describe('health check service', () => {
   it('got state response', () =>
     registerWithManager(sp).then(() => {
       const hs = sp.services['health-check'];
-      return hs.start().then(() => hs.endpoints.state.receive({}).then(r => assert.equal(r, true)));
-    })
-  );
+      return hs
+        .start()
+        .then(() =>
+          hs.endpoints.state.receive({}).then(r => assert.equal(r, true))
+        );
+    }));
 
   it('memory', () =>
     registerWithManager(sp).then(() => {
       const hs = sp.services['health-check'];
-      const re = new SendEndpoint('test', {
-        name: 'a'
-      }, {
-        createOpposite: true
-      });
+      const re = new SendEndpoint(
+        'test',
+        {
+          name: 'a'
+        },
+        {
+          createOpposite: true
+        }
+      );
 
       re.connected = hs.endpoints.memory;
 
-      return hs.start().then(() => re.receive({}).then(response => assert.isAbove(response.heapTotal, 10000)));
-    })
-  );
+      return hs
+        .start()
+        .then(() =>
+          re
+            .receive({})
+            .then(response => assert.isAbove(response.heapTotal, 10000))
+        );
+    }));
 
   it('cpu opposite', () =>
     registerWithManager(sp).then(() => {
       const hs = sp.services['health-check'];
-      const re = new ReceiveEndpoint('test', {
-        name: 'a'
-      }, {
-        createOpposite: true
-      });
+      const re = new ReceiveEndpoint(
+        'test',
+        {
+          name: 'a'
+        },
+        {
+          createOpposite: true
+        }
+      );
 
       let cpuUsage;
       re.receive = message => {
@@ -60,18 +72,27 @@ describe('health check service', () => {
 
       hs.endpoints.cpu.opposite.connected = re;
 
-      return hs.start().then(() => hs.endpoints.cpu.receive({}).then(r => assert.isAbove(cpuUsage.user, 100)));
-    })
-  );
+      return hs
+        .start()
+        .then(() =>
+          hs.endpoints.cpu
+            .receive({})
+            .then(r => assert.isAbove(cpuUsage.user, 100))
+        );
+    }));
 
   it('state opposite', () =>
     registerWithManager(sp).then(() => {
       const hs = sp.services['health-check'];
-      const re = new ReceiveEndpoint('test', {
-        name: 'a'
-      }, {
-        createOpposite: true
-      });
+      const re = new ReceiveEndpoint(
+        'test',
+        {
+          name: 'a'
+        },
+        {
+          createOpposite: true
+        }
+      );
 
       let theState = 77;
 
@@ -81,9 +102,12 @@ describe('health check service', () => {
 
       hs.endpoints.state.opposite.connected = re;
 
-      return hs.start().then(() => hs.endpoints.state.receive({}).then(r => assert.equal(theState, true)));
-    })
-  );
+      return hs
+        .start()
+        .then(() =>
+          hs.endpoints.state.receive({}).then(r => assert.equal(theState, true))
+        );
+    }));
 
   /*
     it('uptime opposite', () =>
