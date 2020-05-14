03ea3c91bcbb1b5d8b03ef199468defd9f475293
Markus Felten <Markus.Felten@gmx.de>
2020-05-14 21:37:47 +0200

    chore: debug

---
+++ tests/helper/debug.mjs
@@ -0,0 +1,26 @@
+import { SendEndpoint } from "@kronos-integration/endpoint";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+import { ServiceHealthCheck } from "@kronos-integration/service-health-check";
+
+async function exec() {
+  const sp = new StandaloneServiceProvider();
+  const hcs = await sp.declareService({
+    type: ServiceHealthCheck,
+    uptimeInterval: 1
+  });
+
+  new SendEndpoint("r", sp, {
+    connected: hcs.endpoints.uptime,
+    receive: response => responses.push(response)
+  });
+
+  await sp.start();
+
+  //await wait(4000);
+}
+
+exec();
+
+async function wait(msecs = 1000) {
+  return new Promise((resolve, reject) => setTimeout(() => resolve(), msecs));
+}
