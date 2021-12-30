7d1ab849ef474d504d9c1ec9bc1a751de6697e4e
Markus Felten <Markus.Felten@gmx.de>
2021-12-30 12:29:01 +0100

    test: use timers/promises instead of home grown wait()

--- tests/helper/debug.mjs
+++ tests/helper/debug.mjs
@@ -1,3 +1,4 @@
+import { setTimeout } from "timers/promises";
 import { SendEndpoint } from "@kronos-integration/endpoint";
 import { StandaloneServiceProvider } from "@kronos-integration/service";
 import { ServiceHealthCheck } from "@kronos-integration/service-health";
@@ -16,11 +17,7 @@ async function exec() {
 
   await sp.start();
 
-  //await wait(4000);
+  //await setTimeout(4000);
 }
 
 exec();
-
-async function wait(msecs = 1000) {
-  return new Promise((resolve, reject) => setTimeout(() => resolve(), msecs));
-}
